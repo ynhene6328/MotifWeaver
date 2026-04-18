f91e000c9c1bb634d98b0cabe349e875b1c6a5ec
NakamuraYuto <ynhene6328@gmail.com>
2026-04-18 22:38:52 +0900

    リファクタリング

--- src/MotifWeaver.Core/Geometry/HexGridGeometry.cs
+++ src/MotifWeaver.Core/Geometry/HexGridGeometry.cs
@@ -11,65 +11,13 @@ namespace MotifWeaver.Core.Geometry;
 /// </summary>
 public sealed class HexGridGeometry : IGridGeometry
 {
-    private readonly float _unitSize;
-    private readonly Dictionary<VertexKey, Vector2> _positionCache;
+    private readonly RegularGridGeometry _inner;
 
     public HexGridGeometry(float unitSize)
     {
-        if (unitSize <= 0.0f)
-        {
-            throw new ArgumentOutOfRangeException(nameof(unitSize));
-        }
-
-        _unitSize = unitSize;
-        _positionCache = new Dictionary<VertexKey, Vector2>();
+        _inner = new RegularGridGeometry(unitSize);
     }
 
-    public Vector2 GetPosition(VertexKey key)
-    {
-        if (_positionCache.TryGetValue(key, out Vector2 cached))
-        {
-            return cached;
-        }
-
-        // 設計書: x = X * (w / 2), y = Y * (√3 / 2) * w
-        float x = key.X * (_unitSize / 2.0f);
-        float y = key.Y * (MathF.Sqrt(3.0f) / 2.0f) * _unitSize;
-
-        Vector2 position = new Vector2(x, y);
-        _positionCache.Add(key, position);
-        return position;
-    }
-
-    public BoundingBox ComputeBounds(IEnumerable<VertexKey> vertexKeys)
-    {
-        if (vertexKeys is null)
-        {
-            throw new ArgumentNullException(nameof(vertexKeys));
-        }
-
-        float minX = float.MaxValue;
-        float minY = float.MaxValue;
-        float maxX = float.MinValue;
-        float maxY = float.MinValue;
-        bool hasAny = false;
-
-        foreach (VertexKey key in vertexKeys)
-        {
-            Vector2 position = GetPosition(key);
-
-            if (position.X < minX) minX = position.X;
-            if (position.Y < minY) minY = position.Y;
-            if (position.X > maxX) maxX = position.X;
-            if (position.Y > maxY) maxY = position.Y;
-            hasAny = true;
-        }
-
-        if (!hasAny)
-        {
-            throw new ArgumentException("At least one vertex key is required.", nameof(vertexKeys));
-        }
-
-        return new BoundingBox(minX, minY, maxX, maxY);
-    }
+    public Vector2 GetPosition(VertexKey key) => _inner.GetPosition(key);
+    public BoundingBox ComputeBounds(IEnumerable<VertexKey> keys) => _inner.ComputeBounds(keys);
 }
